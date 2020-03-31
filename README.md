d4f0c6591011e92254acf040243f279b27a6c075
Thiago Barradas <th.barradas@gmail.com>
2020-03-31 18:34:45 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,93 @@
+# SqlClient.ParseToObject
+
+Get a full object (with nested complex objects) from Microsoft.Data.SqlClient. Created with .NET Core support;
+
+## Install via NuGet
+
+```
+PM> Install-Package SqlClient.ParseToObject
+```
+
+## How to use
+
+Packages:
+```
+Install-Package Microsoft.Data.SqlClient 
+Install-Package Dapper 
+Install-Package SqlClient.ParseToObject 
+```
+
+Optional Packages (to run next code):
+```
+Install-Package Dapper 
+```
+
+Demo Models:
+```
+public class UserModel
+{
+	public string Id { get; set; }
+
+	public string Name { get; set; }
+
+	public int Weight { get; set; }
+
+	public DateTime Birthdate{ get; set; }
+
+	public AddressModel Address { get; set; }
+}
+
+public class AddressModel
+{
+	public string Line1 { get; set; }
+
+	public string Line2 { get; set; }
+
+	public string City { get; set; }
+}
+```
+
+Code:
+```
+
+var query = @"SELECT 
+				U.Cod AS Id,
+				Name,
+				Birthdate,
+				Weight,
+				A.Line1 as [Address.Line1],
+				A.Line2 as [Address.Line2],
+				A.City as [Address.City]
+			  FROM User U INNER JOIN Address A ON U.AddressCod = A.Cod
+			  WHERE Name = @Name";
+
+var parameters = new 
+{
+	Name = "Thiago Barradas"
+};
+
+using (var sqlConnection = new SqlConnection(connectionString))
+{
+    var reader = sqlConnection.ExecuteReader(query, parameters);
+
+    List<User> users = reader.GetResults<User>();
+}
+
+```
+
+## How can I contribute?
+Please, refer to [CONTRIBUTING](.github/CONTRIBUTING.md)
+
+## Found something strange or need a new feature?
+Open a new Issue following our issue template [ISSUE TEMPLATE](.github/ISSUE_TEMPLATE.md)
+
+## Changelog
+See in [nuget version history](https://www.nuget.org/packages/SqlClient.ParseToObject)
+
+## Did you like it? Please, make a donate :)
+
+if you liked this project, please make a contribution and help to keep this and other initiatives, send me some Satochis.
+
+BTC Wallet: `1G535x1rYdMo9CNdTGK3eG6XJddBHdaqfX`
+
+![1G535x1rYdMo9CNdTGK3eG6XJddBHdaqfX](https://i.imgur.com/mN7ueoE.png)
---
+++ SqlClient.ParseToObject/DataReaderExtension.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace System.Data
+{
+    public static class DataReaderExtension
+    {
+        public static List<T> GetResults<T>(this IDataReader reader)
+        {
+            Type returnType = typeof(T);
+
+            PropertyInfo[] returnTypeProperties = returnType.GetProperties(BindingFlags.Instance | BindingFlags.Public);
+
+            DataTable schemaTable = reader.GetSchemaTable();
+
+            List<T> returnCollection = new List<T>();
+
+            TaskFactory taskFactory = new TaskFactory();
+            List<Task> taskList = new List<Task>();
+
+            while (reader.Read() == true)
+            {
+                T returnInstance;
+
+                if (returnType.FullName.IndexOf("System.") >= 0)
+                {
+                    if (reader.IsDBNull(0) == true)
+                    {
+                        continue;
+                    }
+
+                    returnInstance = (T)Convert.ChangeType(reader[0], returnType);
+                    returnCollection.Add(returnInstance);
+                }
+                else
+                {
+                    List<string> mappedProperties = new List<string>();
+                    List<KeyValuePair<string, object>> columns = new List<KeyValuePair<string, object>>();
+
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        columns.Add(new KeyValuePair<string, object>(reader.GetName(i), reader[i]));
+                    }
+
+                    returnInstance = Activator.CreateInstance<T>();
+                    returnCollection.Add(returnInstance);
+
+                    Task task = taskFactory.StartNew(() =>
+                    {
+
+                        for (int i = 0; i < columns.Count; i++)
+                        {
+                            if (columns[i].Value == DBNull.Value || columns[i].Value == null) { continue; }
+                            ParseProperty(returnType, returnTypeProperties, schemaTable, returnInstance, columns[i].Key, columns[i].Value, i, mappedProperties);
+                        }
+                    });
+
+                    taskList.Add(task);
+                }
+            }
+
+            Task.WaitAll(taskList.ToArray());
+
+            return returnCollection;
+        }
+
+        private static bool ParseProperty(Type returnType, PropertyInfo[] returnTypeProperties, DataTable schemaTable, object returnInstance, string columnName, object databaseValue, int ordinal, IList<string> mappedProperties)
+        {
+            if (databaseValue == null) { return false; }
+
+            DataRow dataRow = schemaTable.Select("ColumnName = '" + columnName + "' AND ColumnOrdinal = " + ordinal).FirstOrDefault();
+
+            string tableName = (dataRow != null) ? dataRow["BaseTableName"].ToString() : null;
+
+            PropertyInfo propertyInfo = null;
+
+            string explicitClassName = null;
+            string explicitPropertyName = null;
+
+            if (columnName.IndexOf('.') >= 0)
+            {
+                string[] aliasData = columnName.Split(new char[] { '.' }, 2);
+
+                explicitClassName = aliasData[0];
+                explicitPropertyName = aliasData[1];
+            }
+
+            string propertyName = explicitPropertyName ?? columnName;
+
+            string memberName = explicitClassName ?? propertyName;
+
+            if (string.IsNullOrWhiteSpace(explicitClassName) == true)
+            {
+                if (mappedProperties.Contains(returnType.Name + "." + propertyName) == true)
+                {
+                    return true;
+                }
+            }
+
+            if (string.IsNullOrWhiteSpace(tableName) == true) { tableName = explicitClassName; }
+
+            if (propertyInfo == null)
+            {
+                propertyInfo = returnTypeProperties.FirstOrDefault(p => (p.Name.Equals(tableName, StringComparison.OrdinalIgnoreCase) || p.Name.Equals(memberName, StringComparison.OrdinalIgnoreCase)) && p.PropertyType.FullName.IndexOf("System.") < 0 && p.PropertyType.IsEnum == false);
+
+                if (propertyInfo != null)
+                {
+                    Type subPropertyType = propertyInfo.PropertyType;
+
+                    object subPropertyInstance = propertyInfo.GetValue(returnInstance, null) ?? Activator.CreateInstance(subPropertyType);
+
+                    PropertyInfo[] subPropertyTypeProperties = subPropertyType.GetProperties(BindingFlags.Instance | BindingFlags.Public);
+
+                    if (ParseProperty(subPropertyType, subPropertyTypeProperties, schemaTable, subPropertyInstance, propertyName ?? columnName, databaseValue, ordinal, mappedProperties) == true)
+                    {
+                        propertyInfo.SetValue(returnInstance, subPropertyInstance, null);
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    propertyInfo = returnTypeProperties.FirstOrDefault(p => p.Name.Equals(memberName, StringComparison.OrdinalIgnoreCase));
+                }
+            }
+
+            if (propertyInfo == null)
+            {
+                return false;
+            }
+
+            if (propertyInfo.CanWrite == true)
+            {
+
+                object value = databaseValue;
+                if (propertyInfo.PropertyType.IsEnum == true) { value = Enum.Parse(propertyInfo.PropertyType, databaseValue.ToString(), true); }
+
+                else
+                {
+                    Type propertyType = (propertyInfo.PropertyType.IsGenericType == true) ? propertyInfo.PropertyType.GetGenericArguments()[0] : propertyInfo.PropertyType;
+
+                    if (value.GetType() != propertyType) { value = Convert.ChangeType(value, propertyType); }
+                }
+
+                propertyInfo.SetValue(returnInstance, value, null);
+
+                mappedProperties.Add(returnType.Name + "." + propertyName);
+            }
+
+            return true;
+        }
+    }
+}
